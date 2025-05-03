f3c84ce14933da3921dc4ed505dc0fdf78581181
bhooomyy <bhattbhoomi0111@gmail.com>
2025-05-02 20:25:56 -0400

    Login module designed

---
+++ Login.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import hashlib
+user_database='/Users/bhoomi/Documents/GitHub/DBMS/user_database.csv'
+
+def login():
+    username=input('Enter username: ')
+    password=input('Enter password: ')
+    password=hashlib.sha256(password.encode()).hexdigest()
+    df=pd.read_csv(user_database)
+    if username in df['username'].values:
+        row=df[df['username']==username]
+        if password==row.iloc[0]['password']:
+            print(f'Welcome back {username}')
+    else:
+        print('Invalid username or password')
+
